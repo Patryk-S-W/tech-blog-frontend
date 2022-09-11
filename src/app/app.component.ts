e87430a345f8891f2a038096eb336d8fbfa80478
Patryk-S-W <10290008+Patryk-S-W@users.noreply.github.com>
2022-09-11 06:01:00 +0000

    fix: typo in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {dxad
+export class AppComponent {
   title = 'tech-blog';
 }
