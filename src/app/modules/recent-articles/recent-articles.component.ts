6787d2f369ae944732f6ce26b9bdd43b05354fdc
Patryk-S-W <10290008+Patryk-S-W@users.noreply.github.com>
2022-10-28 19:26:00 +0000

    feat: add recent articles

--- src/app/modules/recent-articles/recent-articles.component.ts
+++ src/app/modules/recent-articles/recent-articles.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class RecentArticlesComponent {
 
+  articles = [
+    {
+      title: "Podcast Title",
+      image: "https://picsum.photos/1000/1000",
+      date: "Mon, May 25th 2020",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero.Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!",
+      category: "Podcast",
+      duration: "55 mins.",
+      button: "Play Episode",
+    },
+    {
+      title: "Podcast Title",
+      image: "https://picsum.photos/1000/1000",
+      date: "Mon, May 25th 2020",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero.Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!",
+      category: "Podcast",
+      duration: "55 mins.",
+      button: "Play Episode",
+    },
+    {
+      title: "Podcast Title",
+      image: "https://picsum.photos/1000/1000",
+      date: "Mon, May 25th 2020",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero.Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!",
+      category: "Podcast",
+      duration: "55 mins.",
+      button: "Play Episode",
+    },
+  ];
 }
