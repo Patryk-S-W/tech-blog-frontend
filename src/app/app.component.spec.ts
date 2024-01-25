013e63028769a7cbe566ae86c31b491b19e17836
Patryk-S-W <10290008+Patryk-S-W@users.noreply.github.com>
2024-01-25 09:59:04 +0100

    chore: update readme, add changelg

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'tech-blog'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tech-blog');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'tech-blog app is running!'
-    );
-  });
 });
